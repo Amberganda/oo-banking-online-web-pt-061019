86b87a7f509d7a5635b97c8b3a8be1f70c3fab5c
Amber Taranto <ambzzzz@gmail.com>
2019-08-06 01:30:31 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,15 +1,17 @@
 class BankAccount
 
     attr_reader :name
-    attr_accessor :balance, :status
+    attr_accessor :balance, :status, :amount
 
 
     def initialize(name)
         @name = name
         @balance = 1000
         @status = "open"
+    end
 
-
+    def deposit(amount)
+        @amount = amount
     end
 
 
