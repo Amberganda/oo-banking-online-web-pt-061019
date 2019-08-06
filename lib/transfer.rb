d0ce5723700d5bcb87dfcd4c007634a16670456a
Amber Taranto <ambzzzz@gmail.com>
2019-08-06 01:45:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,11 @@
 class Transfer
 
-    attr_accessor :sender
+    attr_accessor :sender, :receiver, :status, :transfer_amount
 
     def initialize(sender)
         @sender = sender
-
-
+        @receiver = receiver
+        @status = "pending"
+        @transfer_amount = transfer_amount
     end
 end
